0aafc94b6677943ce251a9bb1b22a00fdb01a175
sam <mail@samueljenks.me>
2024-02-04 15:40:50 +1300

    feat: credentials in environment

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 echo "creating resource group"
 call az group create --location australiaeast --resource-group b4ms-vm-group
 echo "deploying resources"
-call az deployment group create --name b4ms-vm-group --resource-group b4ms-vm-group --template-file ./b4ms-vm-group.json
+call az deployment group create --name b4ms-vm-deployment --resource-group b4ms-vm-group --template-file ./b4ms-vm-group.json --parameters adminUsername=%AZ_USER% adminPassword=%AZ_PASS%
