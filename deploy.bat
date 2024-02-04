0fe754ed77bad24577c47e6f040865a7e6292c2f
sam <mail@samueljenks.me>
2024-02-04 13:56:26 +1300

    refactor: names

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-az group deploymente create --name b4ms-vm-test --resource-group b4ms-vm-test --template-file ./b4ms-vm.json
+az group deploymente create --name b4ms-vm --resource-group b4ms-vm-group --template-file ./b4ms-vm-group.json
