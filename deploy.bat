5a2d9b2da82c5d1e1a1bb6d4213045d843d4f2b4
sam <mail@samueljenks.me>
2024-02-04 13:58:06 +1300

    fix: spelling

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-az group deploymente create --name b4ms-vm --resource-group b4ms-vm-group --template-file ./b4ms-vm-group.json
+az group deployment create --name b4ms-vm --resource-group b4ms-vm-group --template-file ./b4ms-vm-group.json
